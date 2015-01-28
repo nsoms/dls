e6c0b9a6b97efdc994ba12e6cff21c2bdf913cf1
Nick Soms <n.l.soms@gmail.com>
2015-01-28 11:46:09 +0300

    updated database structure

--- db/scheme.sql
+++ db/scheme.sql
@@ -9,27 +9,31 @@ DROP TABLE IF EXISTS sysvariables CASCADE;
 
 CREATE PROCEDURAL LANGUAGE plpgsql;
 
+-- таблица ролей
 CREATE TABLE roles(
     id              serial PRIMARY KEY,
     rolename        name NOT NULL,
     
     UNIQUE(rolename)
 );
-
 INSERT INTO roles(rolename) VALUES ('admin', 'user', 'guest');
 
+
+-- таблица пользователей
 CREATE TABLE users(
     id              serial PRIMARY KEY,
-    surname         text not null,
-    name            text not null,
-    middlename      text not null,
-    birthday        date not null default '1970-01-01',
-    register        timestamp with time zone default now(),
-    reg_form        text,
-    login           name,
-    passwd          text,
-    role_id         int default 3, -- guest
-    is_disabled     bool default false,
+    card_number     text,                                   -- номер карты
+    surname         text not null,                          -- фамилия
+    name            text not null,                          -- имя
+    middlename      text not null,                          -- отчество
+    pic_name        text,                                   -- имя файла фотографии
+    birthday        date not null default '1970-01-01',     -- дата рождения
+    register        timestamp with time zone default now(), -- дата регистрации
+    reg_form        text,                                   -- класс регистрации
+    login           name,                                   -- логин для авторизации
+    passwd          text,                                   -- пароль
+    role_id         int default 3,                          -- guest по-умолчанию
+    is_disabled     bool default false,                     -- включен/выключен
     UNIQUE (surname, name, reg_form, birthday),
     FOREIGN KEY(role_id) REFERENCES roles
 );
@@ -38,7 +42,22 @@ CREATE UNIQUE INDEX users_login_uni_idx ON users(login) WHERE login IS NOT NULL;
 INSERT INTO users(display_name,login,passwd,role_id)
     VALUES ('Администратор', 'admin', '08bc0e80658615a6bd4753f279d45c0fb495c60bf64309a90f8313fea61757a1', 1);
 
+-- таблица групп пользователей
+CREATE TABLE groups(
+    id              serial PRIMARY KEY,
+    name            text
+);
 
+-- талица отнесенности пользователей к группе
+CREATE TABLE user_groups(
+    user_id         int,
+    group_id        int,
+    FOREIGN KEY(user_id) REFERENCES users,
+    FOREIGN KEY(group_id) REFERENCES groups
+);
+
+
+-- системная таблица
 CREATE TABLE sysvariables(
     var_name    name,
     var_data    text,
