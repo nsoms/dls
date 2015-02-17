61fdb228d03422b8ec0b02f834fb5b8e872c1bc0
Nick Soms <n.l.soms@gmail.com>
2015-02-17 10:41:27 +0300

    improved import script

--- db/import.sh
+++ db/import.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-php import.php 8-1_import.csv 08-1 Ученик
-php import.php 8-2_import.csv 08-2 Ученик
-php import.php 8-3_import.csv 08-3 Ученик
-php import.php 9-1_import.csv 09-1 Ученик
-php import.php 9-2_import.csv 09-2 Ученик
-php import.php 9-3_import.csv 09-3 Ученик
-php import.php 9-4_import.csv 09-4 Ученик
-php import.php 9-5_import.csv 09-5 Ученик
-php import.php 10-1_import.csv 10-1 Ученик
-php import.php 10-2_import.csv 10-2 Ученик
-php import.php 10-3_import.csv 10-3 Ученик
-php import.php 10-4_import.csv 10-4 Ученик
-php import.php 10-5_import.csv 10-5 Ученик
-php import.php 10-6_import.csv 10-6 Ученик
-php import.php 11-1_import.csv 11-1 Ученик
-php import.php 11-2_import.csv 11-2 Ученик
-php import.php 11-3_import.csv 11-3 Ученик
-php import.php 11-4_import.csv 11-4 Ученик
-php import.php 11-5_import.csv 11-5 Ученик
+
+php import.php forms/8-1_import.csv 08-1 Ученик
+php import.php forms/8-2_import.csv 08-2 Ученик
+php import.php forms/8-3_import.csv 08-3 Ученик
+php import.php forms/9-1_import.csv 09-1 Ученик
+php import.php forms/9-2_import.csv 09-2 Ученик
+php import.php forms/9-3_import.csv 09-3 Ученик
+php import.php forms/9-4_import.csv 09-4 Ученик
+php import.php forms/9-5_import.csv 09-5 Ученик
+php import.php forms/10-1_import.csv 10-1 Ученик
+php import.php forms/10-2_import.csv 10-2 Ученик
+php import.php forms/10-3_import.csv 10-3 Ученик
+php import.php forms/10-4_import.csv 10-4 Ученик
+php import.php forms/10-5_import.csv 10-5 Ученик
+php import.php forms/10-6_import.csv 10-6 Ученик
+php import.php forms/11-1_import.csv 11-1 Ученик
+php import.php forms/11-2_import.csv 11-2 Ученик
+php import.php forms/11-3_import.csv 11-3 Ученик
+php import.php forms/11-4_import.csv 11-4 Ученик
+php import.php forms/11-5_import.csv 11-5 Ученик
